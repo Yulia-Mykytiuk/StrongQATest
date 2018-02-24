efcb29c3ce42e05c3b6d41da152f2f4855b5329e
YuliaMykytiuk <julia4422@gmail.com>
2018-02-25 01:25:50 +0200

    final

--- scripts/common.js
+++ scripts/common.js
@@ -68,41 +68,6 @@ function getCookie(cname) {
 	return "";
 }
 
-function setCookie(name, value, options) {
-	options = options || {};
-
-	var expires = options.expires;
-
-	if (typeof expires == "number" && expires) {
-		var d = new Date();
-		d.setTime(d.getTime() + expires * 1000);
-		expires = options.expires = d;
-	}
-	if (expires && expires.toUTCString) {
-		options.expires = expires.toUTCString();
-	}
-
-	value = encodeURIComponent(value);
-
-	var updatedCookie = name + "=" + value;
-
-	for (var propName in options) {
-		updatedCookie += "; " + propName;
-		var propValue = options[propName];
-		if (propValue !== true) {
-			updatedCookie += "=" + propValue;
-		}
-	}
-
-	document.cookie = updatedCookie;
-}
-
-function deleteCookie(name) {
-	setCookie(name, "", {
-		expires: -1
-	});
-}
-
 $(document).ready(function () {
 
 	setTimeout(function () {
@@ -140,7 +105,3 @@ $(document).ready(function () {
 	}
 
 });
-
-$(window).load(function () {});
-
-$(window).resize(function () {});
